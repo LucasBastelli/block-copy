860d84807ba8e1cf03865f920169fa286cae0e02
Lucas <lbspagnol@gmail.com>
2023-02-27 18:33:10 -0300

    Adding the program

---
+++ README.md
@@ -0,0 +1,8 @@
+The program in C use fread nd fwrite to copy a file to another directory
+to compile:
+
+    gcc -o blkcopy blkcopy.c
+
+to use:
+
+    ./blkcopy InputFile OutputFile BlockSize
---
+++ blkcopy.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[]){
+    if (argc < 4)
+    {
+        printf("Missing something\n");
+        printf("USAGE: file output_file sizeof_blocks\n");
+        return(1);
+    }
+    char *infile= argv[1];
+    char *outfile= argv[2];
+    int blocksize = atoi(argv[3]);
+    if(blocksize==0){
+        printf("invalid size number\n");
+        return(1);
+    }
+    signed char buf[100];
+    const void *ptr = (void *) buf;
+    FILE *input, *output;
+    size_t n;
+    input = fopen(infile, "r");
+    if (!input) {
+        printf("Unable to open input file\n");
+        return(1);
+    }
+    output = fopen(outfile, "w");
+    if (!output) {
+        printf("Unable to open output file\n");
+        return(1);
+    }
+    while ((n = fread(ptr, sizeof(char), blocksize, input)) == blocksize){
+        fwrite(ptr, sizeof(char), blocksize, output);
+    }
+    fwrite(ptr, sizeof(char), n, output);
+    pclose(input);
+    pclose(output);
+    return 0;
+}
